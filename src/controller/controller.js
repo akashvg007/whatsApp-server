11caf69ff5a08ca89b787260aae95af30bfa324c
Akash VG <akashvg007@gmail.com>
2021-11-27 15:18:15 +0530

    api to update name

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -15,227 +15,235 @@ config();
 const { ACCESS_TOKEN_SECRED, REFRESH_TOKEN } = process.env;
 
 const generateAccessToken = (user) => {
-    const data = typeof user === "object" ? user : { user };
-    return jwt.sign(data, ACCESS_TOKEN_SECRED, { expiresIn: "365d" });
+  const data = typeof user === "object" ? user : { user };
+  return jwt.sign(data, ACCESS_TOKEN_SECRED, { expiresIn: "365d" });
 };
 
 const ContactList = {};
 
 export const registerOrLogin = async (req, res) => {
-    try {
-        const { name, phone } = req.body;
-        const user = await User.find({ phone });
-        console.log("result is ", user, phone);
-        if (!user) await User.create({ name, phone })
-        const data = sendOTP(res, phone);
-        sendResponse(false, "OTP send", res, 200, data);
-    }
-    catch (err) {
-        console.log("registerOrLogin::catch", err.message);
-        sendResponse(true, 104, res, 500);
-    }
-}
+  try {
+    const { name, phone } = req.body;
+    const user = await User.find({ phone });
+    console.log("result is ", user, phone);
+    if (!user) await User.create({ name, phone });
+    const data = sendOTP(res, phone);
+    sendResponse(false, "OTP send", res, 200, data);
+  } catch (err) {
+    console.log("registerOrLogin::catch", err.message);
+    sendResponse(true, 104, res, 500);
+  }
+};
 
 export const verify = async (req, res) => {
-    try {
-        const { otp, phone } = req.body;
-        const payload = { code: otp, to: phone }
-        const data = await verifyOTP(res, payload)
-        //bypassing the otp
-        if (otp !== "0000") {
-            if (!data || !data.status || data.status !== "approved") {
-                sendResponse(true, 105, res, 400);
-                return;
-            }
-        }
-        const query = { phone };
-        const newData = { active: true };
-        const upsert = { upsert: true }
-        await User.findOneAndUpdate(query, newData, upsert);
-        const accessToken = generateAccessToken(phone);
-        const refreshToken = jwt.sign(phone, REFRESH_TOKEN);
-        console.log("tokens", accessToken, refreshToken);
-        // await Token.create({ refreshToken, phone })
-        const token = { accessToken, refreshToken };
-        sendResponse(false, "Logged in successful", res, 200, token);
+  try {
+    const { otp, phone } = req.body;
+    const payload = { code: otp, to: phone };
+    const data = await verifyOTP(res, payload);
+    //bypassing the otp
+    if (otp !== "0000") {
+      if (!data || !data.status || data.status !== "approved") {
+        sendResponse(true, 105, res, 400);
+        return;
+      }
     }
-    catch (err) {
-        console.log("verify::catch", err.message);
-        sendResponse(true, 104, res, 500);
-    }
-}
+    const query = { phone };
+    const newData = { active: true };
+    const upsert = { upsert: true };
+    await User.findOneAndUpdate(query, newData, upsert);
+    const accessToken = generateAccessToken(phone);
+    const refreshToken = jwt.sign(phone, REFRESH_TOKEN);
+    console.log("tokens", accessToken, refreshToken);
+    // await Token.create({ refreshToken, phone })
+    const token = { accessToken, refreshToken };
+    sendResponse(false, "Logged in successful", res, 200, token);
+  } catch (err) {
+    console.log("verify::catch", err.message);
+    sendResponse(true, 104, res, 500);
+  }
+};
+
+export const updateNameAndDP = async (req, res) => {
+  try {
+    const { name, phone } = req.body;
+    const query = { phone };
+    const newData = { name };
+    const upsert = { upsert: true };
+    await User.findOneAndUpdate(query, newData, upsert);
+    sendResponse(false, "updated", res, 200);
+  } catch (err) {
+    console.log("verify::catch", err.message);
+    sendResponse(true, 104, res, 500);
+  }
+};
 
 export const sendMessage = async (req, res) => {
-    try {
-        await Chat.create(req.body);
-        sendResponse(false, "message send", res, 200);
-    }
-    catch (err) {
-        console.log("sendMessage::catch", err.message);
-        sendResponse(true, 104, res, 500);
-    }
-}
+  try {
+    await Chat.create(req.body);
+    sendResponse(false, "message send", res, 200);
+  } catch (err) {
+    console.log("sendMessage::catch", err.message);
+    sendResponse(true, 104, res, 500);
+  }
+};
 export const getMessage = async (req, res) => {
-    try {
-        const { from, to, pagination, lastTime = 0 } = req.body;
-        const query = {
-            $and: [
-                { $or: [{ from, to }, { from: to, to: from }] },
-                { time: { $gt: lastTime } }
-            ]
-        }
-        const result = await Chat.find(query).sort({ _id: -1 });
-        sendResponse(false, "message send", res, 200, result);
-    }
-    catch (err) {
-        console.log("getMessage::catch", err.message);
-        sendResponse(true, 104, res, 500);
-    }
-}
+  try {
+    const { from, to, pagination, lastTime = 0 } = req.body;
+    const query = {
+      $and: [
+        {
+          $or: [
+            { from, to },
+            { from: to, to: from },
+          ],
+        },
+        { time: { $gt: lastTime } },
+      ],
+    };
+    const result = await Chat.find(query).sort({ _id: -1 });
+    sendResponse(false, "message send", res, 200, result);
+  } catch (err) {
+    console.log("getMessage::catch", err.message);
+    sendResponse(true, 104, res, 500);
+  }
+};
 
 export const getRecent = async (req, res) => {
-    try {
-        const from = req.user;
-        const { lastTime } = req.params;
-        if (!ContactList[from]) ContactList[from] = [];
-        console.log("getRecent::from", from);
-        const query = {
-            $and: [
-                { $or: [{ from }, { to: from }] },
-                { time: { $gt: lastTime || 0 } }
-            ]
-        }
-        const chats = await Chat.find(query).sort({ _id: -1 });
-        // const mappedData = {};
-        // chats.forEach(x => {
-        //     if (x.from === from) {
-        //         if (!mappedData[x.to]) mappedData[x.to] = []
-        //         mappedData[x.to].push(x);
-        //     }
-        //     else {
-        //         if (!mappedData[x.from]) mappedData[x.from] = []
-        //         mappedData[x.from].push(x);
-        //     }
-        // });
-        // ContactList[from] = Object.keys(mappedData)
-        // const lastMessage = Object.keys(mappedData).map((key) => mappedData[key][0])
-        sendResponse(false, "", res, 200, chats);
-    }
-    catch (err) {
-        console.log("getRecent::catch", err.message);
-        sendResponse(true, 104, res, 500);
-    }
-}
+  try {
+    const from = req.user;
+    const { lastTime } = req.params;
+    if (!ContactList[from]) ContactList[from] = [];
+    console.log("getRecent::from", from);
+    const query = {
+      $and: [
+        { $or: [{ from }, { to: from }] },
+        { time: { $gt: lastTime || 0 } },
+      ],
+    };
+    const chats = await Chat.find(query).sort({ _id: -1 });
+    // const mappedData = {};
+    // chats.forEach(x => {
+    //     if (x.from === from) {
+    //         if (!mappedData[x.to]) mappedData[x.to] = []
+    //         mappedData[x.to].push(x);
+    //     }
+    //     else {
+    //         if (!mappedData[x.from]) mappedData[x.from] = []
+    //         mappedData[x.from].push(x);
+    //     }
+    // });
+    // ContactList[from] = Object.keys(mappedData)
+    // const lastMessage = Object.keys(mappedData).map((key) => mappedData[key][0])
+    sendResponse(false, "", res, 200, chats);
+  } catch (err) {
+    console.log("getRecent::catch", err.message);
+    sendResponse(true, 104, res, 500);
+  }
+};
 
 export const addToContact = async (req, res) => {
-    try {
-        const from = req.user;
-        const { phone, name } = req.body;
-        const contact = await Contact.create({ name, phone, from });
-        sendResponse(false, "", res, 200, contact);
-    }
-    catch (err) {
-        console.log("addToContact::catch", err.message);
-        sendResponse(true, 104, res, 500);
-    }
-}
+  try {
+    const from = req.user;
+    const { phone, name } = req.body;
+    const contact = await Contact.create({ name, phone, from });
+    sendResponse(false, "", res, 200, contact);
+  } catch (err) {
+    console.log("addToContact::catch", err.message);
+    sendResponse(true, 104, res, 500);
+  }
+};
 
 export const getMyContacts = async (req, res) => {
-    try {
-        const from = req.user;
-        const contacts = await Contact.find({ from });
-        contacts.forEach((contact) => {
-            const { phone } = contact;
-            if (ContactList[from] && !ContactList[from].includes(phone)) {
-                ContactList[from].push(phone)
-            }
-        })
-        sendResponse(false, "", res, 200, contacts);
-    }
-    catch (err) {
-        console.log("getMyContacts::catch", err.message);
-        sendResponse(true, 104, res, 500);
-    }
-}
+  try {
+    const from = req.user;
+    const contacts = await Contact.find({ from });
+    contacts.forEach((contact) => {
+      const { phone } = contact;
+      if (ContactList[from] && !ContactList[from].includes(phone)) {
+        ContactList[from].push(phone);
+      }
+    });
+    sendResponse(false, "", res, 200, contacts);
+  } catch (err) {
+    console.log("getMyContacts::catch", err.message);
+    sendResponse(true, 104, res, 500);
+  }
+};
 export const updateProfilePic = async (req, url) => {
-    try {
-        const phone = req.user;
-        const query = { phone }
-        const newData = { profilePic: url };
-        const upsert = { upsert: true }
-        await User.findOneAndUpdate(query, newData, upsert);
-    }
-    catch (err) {
-        console.log("getMyContacts::catch", err.message);
-        throw err;
-    }
-}
+  try {
+    const phone = req.user;
+    const query = { phone };
+    const newData = { profilePic: url };
+    const upsert = { upsert: true };
+    await User.findOneAndUpdate(query, newData, upsert);
+  } catch (err) {
+    console.log("getMyContacts::catch", err.message);
+    throw err;
+  }
+};
 export const updateLastSeen = async (req, res) => {
-    try {
-        const phone = req.user;
-        const query = { phone }
-        const lastseen = Date.now();
-        const newData = { lastseen };
-        const upsert = { upsert: true }
-        await User.findOneAndUpdate(query, newData, upsert);
-        sendResponse(false, "", res, 200);
-    }
-    catch (err) {
-        console.log("updateLastSeen::catch", err.message);
-        throw err;
-    }
-}
+  try {
+    const phone = req.user;
+    const query = { phone };
+    const lastseen = Date.now();
+    const newData = { lastseen };
+    const upsert = { upsert: true };
+    await User.findOneAndUpdate(query, newData, upsert);
+    sendResponse(false, "", res, 200);
+  } catch (err) {
+    console.log("updateLastSeen::catch", err.message);
+    throw err;
+  }
+};
 export const getLastSeen = async (req, res) => {
-    try {
-        const { phone } = req.body;
-        const result = await User.find({ phone });
-        sendResponse(false, "", res, 200, result);
-    }
-    catch (err) {
-        console.log("getLastSeen::catch", err.message);
-        throw err;
-    }
-}
+  try {
+    const { phone } = req.body;
+    const result = await User.find({ phone });
+    sendResponse(false, "", res, 200, result);
+  } catch (err) {
+    console.log("getLastSeen::catch", err.message);
+    throw err;
+  }
+};
 
 export const getAllMyUsers = async (req, res) => {
-    try {
-        const { phones=[] } = req.body;
-        const result = await User.find({ phone:{$in:phones} });
-        sendResponse(false, "", res, 200, result);
-    }
-    catch (err) {
-        console.log("getLastSeen::catch", err.message);
-        throw err;
-    }
-}
+  try {
+    const { phones = [] } = req.body;
+    const result = await User.find({ phone: { $in: phones } });
+    sendResponse(false, "", res, 200, result);
+  } catch (err) {
+    console.log("getLastSeen::catch", err.message);
+    throw err;
+  }
+};
 
 export const getAllMyUserDetails = async (req, res) => {
-    try {
-        const from = req.user;
-        console.log("contactlist", ContactList);
+  try {
+    const from = req.user;
+    console.log("contactlist", ContactList);
 
-        if (ContactList && !ContactList[from] || ContactList[from].length == 0) {
-            ContactList[from] = [];
-            const query = { $or: [{ from }, { to: from }] }
-            const chats = await Chat.find(query).sort({ _id: -1 });
-            const mappedData = {};
-            chats.forEach(x => {
-                if (x.from === from) {
-                    if (!mappedData[x.to]) mappedData[x.to] = []
-                    mappedData[x.to].push(x);
-                }
-                else {
-                    if (!mappedData[x.from]) mappedData[x.from] = []
-                    mappedData[x.from].push(x);
-                }
-            });
-            ContactList[from] = Object.keys(mappedData)
+    if ((ContactList && !ContactList[from]) || ContactList[from].length == 0) {
+      ContactList[from] = [];
+      const query = { $or: [{ from }, { to: from }] };
+      const chats = await Chat.find(query).sort({ _id: -1 });
+      const mappedData = {};
+      chats.forEach((x) => {
+        if (x.from === from) {
+          if (!mappedData[x.to]) mappedData[x.to] = [];
+          mappedData[x.to].push(x);
+        } else {
+          if (!mappedData[x.from]) mappedData[x.from] = [];
+          mappedData[x.from].push(x);
         }
-        const contacts = await User.find({ phone: { $in: [from, ...ContactList[from]] } })
-        sendResponse(false, "", res, 200, contacts);
+      });
+      ContactList[from] = Object.keys(mappedData);
     }
-    catch (err) {
-        console.log("getMyContacts::catch", err.message);
-        sendResponse(true, 104, res, 500);
-    }
-}
+    const contacts = await User.find({
+      phone: { $in: [from, ...ContactList[from]] },
+    });
+    sendResponse(false, "", res, 200, contacts);
+  } catch (err) {
+    console.log("getMyContacts::catch", err.message);
+    sendResponse(true, 104, res, 500);
+  }
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,80 +1,94 @@
 import { Router } from "express";
 import { sendResponse } from "../Response/Response";
 import {
-    registerOrLogin, verify, sendMessage, getRecent, addToContact, getMessage,
-    getMyContacts, updateProfilePic, getAllMyUserDetails, updateLastSeen, getLastSeen,
-    getAllMyUsers
+  registerOrLogin,
+  verify,
+  sendMessage,
+  getRecent,
+  addToContact,
+  getMessage,
+  getMyContacts,
+  updateProfilePic,
+  getAllMyUserDetails,
+  updateLastSeen,
+  getLastSeen,
+  getAllMyUsers,
+  updateNameAndDP,
 } from "../controller/controller";
 import { uploadFileBuffer } from "../helper/s3";
-import { authenticateToken } from "../middlewares/auth"
+import { authenticateToken } from "../middlewares/auth";
 import multer from "multer";
 
 const router = Router();
 
 const fileStorageEngine = multer.memoryStorage({
-    destination: (req, file, cb) => {
-        cb(null, "");
-    },
+  destination: (req, file, cb) => {
+    cb(null, "");
+  },
 });
 const upload = multer({ storage: fileStorageEngine }).single("file");
 
 router.post("/register", (req, res) => {
-    registerOrLogin(req, res);
+  registerOrLogin(req, res);
 });
 
 router.post("/upload", authenticateToken, upload, async (req, res) => {
-    try {
-        const { file } = req;
-        console.log("inside the upload file", file);
-        const result = await uploadFileBuffer(file);
-        await updateProfilePic(req, result?.Location)
-        sendResponse(false, "", res, 200, { url: result?.Location });
-    } catch (err) {
-        console.log("err in catch::updatePhoto", err.message);
-        sendResponse(true, 101, res, 500);
-    }
+  try {
+    const { file } = req;
+    console.log("inside the upload file", file);
+    const result = await uploadFileBuffer(file);
+    await updateProfilePic(req, result?.Location);
+    sendResponse(false, "", res, 200, { url: result?.Location });
+  } catch (err) {
+    console.log("err in catch::updatePhoto", err.message);
+    sendResponse(true, 101, res, 500);
+  }
+});
+
+router.post("/update/name", (req, res) => {
+  updateNameAndDP(req, res);
 });
 
 router.post("/verify", (req, res) => {
-    verify(req, res);
+  verify(req, res);
 });
 
 router.post("/getAllContacts", (req, res) => {
-    getAllMyUsers(req, res);
+  getAllMyUsers(req, res);
 });
 
 router.post("/send-msg", authenticateToken, (req, res) => {
-    sendMessage(req, res);
+  sendMessage(req, res);
 });
 
 router.post("/get-msg", authenticateToken, (req, res) => {
-    getMessage(req, res);
+  getMessage(req, res);
 });
 router.post("/addcontact", authenticateToken, (req, res) => {
-    addToContact(req, res);
+  addToContact(req, res);
 });
 
 router.get("/getrecent", authenticateToken, (req, res) => {
-    getRecent(req, res);
+  getRecent(req, res);
 });
 router.get("/getrecent/:lastTime", authenticateToken, (req, res) => {
-    getRecent(req, res);
+  getRecent(req, res);
 });
 
 router.post("/update-last-seen", authenticateToken, (req, res) => {
-    updateLastSeen(req, res);
+  updateLastSeen(req, res);
 });
 
 router.post("/get-last-seen", authenticateToken, (req, res) => {
-    getLastSeen(req, res);
+  getLastSeen(req, res);
 });
 
 router.get("/getcontacts", authenticateToken, (req, res) => {
-    getMyContacts(req, res);
+  getMyContacts(req, res);
 });
 
 router.get("/getAllMyUserDetails", authenticateToken, (req, res) => {
-    getAllMyUserDetails(req, res);
+  getAllMyUserDetails(req, res);
 });
 
 export default router;
